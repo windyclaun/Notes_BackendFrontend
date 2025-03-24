2ad12983ec4b84140f2e18a7df0411f003337bd7
windyclaun <windynapitupulu3108@gmail.com>
2025-03-24 08:53:07 +0700

    database update

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -3,14 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        dialect: "mysql",
-    }
-);
+const db = new Sequelize("notes_db", "root", "", {
+  host: "34.170.151.206",
+  dialect: "mysql"
+});
 
 export default db;
